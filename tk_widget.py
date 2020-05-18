eeb0f70d9bf62c425f0758eba106151796bfb715
Konstantin Matveev <uragankm@mail.ru>
2020-05-18 16:38:01 +0300

    basic tk frame interaction

--- tk_widget.py
+++ tk_widget.py
@@ -40,8 +40,13 @@ class Program(tk.Tk):
 
         self.show_frame(StartPage)
 
-    def show_frame(self, cont):
-        frame = self.frames[cont]
+    def show_frame(self, type, func=None, cond=None):
+        frame = self.frames[type]
+
+        # set_attr if frame has such method
+        set_attr_method = getattr(frame, "set_attr", None)
+        if callable(set_attr_method):
+            frame.set_attr(func, cond)
         frame.tkraise()
 
 
@@ -49,24 +54,58 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
+        self.controller = controller
+        self.label = tk.Label(self, text="Solve Runge Kutta", font=LARGE_FONT)
+        self.label.pack(pady=10, padx=10)
 
-        button = ttk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
-        button.pack()
+        self.ftext = tk.Label(self, text="Insert function (e. g. y' = x*y)")
+        self.ftext.pack()
+        self.fedit = tk.Entry(self)
+        self.fedit.pack(pady=5)
+
+        self.ctext = tk.Label(self, text="Insert condition (e. g. y(0) = 2)")
+        self.ctext.pack()
+        self.cedit = tk.Entry(self)
+        self.cedit.pack(pady=5)
+
+        self.button = ttk.Button(self, text="Solve",
+                                 command=self.solve_listener)
+        self.button.pack()
+
+    def solve_listener(self):
+        func = self.fedit.get()
+        cond = self.cedit.get()
+        self.controller.show_frame(PageOne, func, cond)
 
 
 class PageOne(tk.Frame):
 
+    """
+    func, cond
+    """
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
+        self.label = tk.Label(self, text="Result", font=LARGE_FONT)
+        self.label.pack(pady=10, padx=10)
+
+        self.res_shower = tk.Text(self, wrap=tk.WORD)
+        self.res_shower.pack(pady=5)
 
         button1 = ttk.Button(self, text="Back to Home",
                              command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
+    def set_attr(self, func=None, cond=None):
+        self.func = func
+        self.cond = cond
+        self.res_shower.delete(1., tk.END)
+        self.res_shower.insert(1., self.func + '\n' + self.cond)
+        self.res_shower.config(
+            width=max(15, 1 + max(len(self.func), len(self.cond))),
+            height=1+2
+        )
 
-Program().mainloop()
+
+if __name__ == '__main__':
+    Program().mainloop()
